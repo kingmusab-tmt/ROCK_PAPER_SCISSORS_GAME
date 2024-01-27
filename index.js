977a6b993f9dc3b11d40298798d844c869425f42
kingmusab-tmt <musab.buraimoh@gmail.com>
2024-01-27 09:34:47 +0100

    ui part

--- index.js
+++ index.js
@@ -20,26 +20,61 @@ function playRound(playerSelection, computerSelection) {
   } else return "You lost";
 }
 
-function game() {
-  let live = 5;
+function game(userChoice) {
+  // let live = 5;
 
-  do {
-    console.log(`You have ${live} left`);
+  // do {
+    // console.log(`You have ${live} left`);
     const computerSelection = getComputerChoice();
-    const playerSelection = prompt("Enter your choice: ").toLocaleLowerCase();
+    const playerSelection = userChoice; 
     console.log("Computer choice is " + computerSelection);
     console.log("Your choice is " + playerSelection);
     let result = playRound(playerSelection, computerSelection);
-
+    
     if (result == "It's a tie" || result == "You won") {
+      umanipulate(result, computerSelection, playerSelection);
       console.log(result);
     } else {
-      live--;
+      // live--;
+      umanipulate(result, computerSelection, playerSelection);
       console.log(result);
-      if (live == 0) {
+      // if (live == 0) {
         console.log("Game Over");
-      }
+      // }
     }
-  } while (live > 0);
+  // } while (live > 0);
 }
-game();
+const buttons = document.querySelectorAll(".buttons");
+  buttons.forEach((buttons) =>{
+
+    buttons.addEventListener('click', function (e) {
+          const userChoice = buttons.id;
+          const clearAll = document.querySelector("#result-container")
+          clearAll.textContent = " "
+          game(userChoice);
+      });
+  })  
+      
+
+function umanipulate(result, computerSelection, playerSelection){
+  const outcome = document.querySelector("#result-container");
+  const div = document.createElement("div");
+      div.className = "result";
+      div.id = "comchoice";
+      div.textContent = result;
+      const firstChoice = document.createElement("div");
+      firstChoice.className = "result";
+      const compImage = document.createElement("img")
+      compImage.className = "resultimage";
+      compImage.src = `${computerSelection}.jpg`;
+      const secondChoice = document.createElement("div");
+      secondChoice.className = "result";
+      const playImage = document.createElement("img")
+      playImage.className = "resultimage";
+      playImage.src = `${playerSelection}.jpg`;
+      secondChoice.appendChild(playImage)
+      firstChoice.appendChild(compImage);
+      outcome.appendChild(firstChoice);
+      outcome.appendChild(div);
+      outcome.appendChild(secondChoice)
+}
